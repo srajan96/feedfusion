63ea966728df96f668279e6e72039873ddddf508
srajan96 <srajan1996@gmail.com>
2017-03-13 00:22:05 +0530

    testing post-recieve,made changes on local #2

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -16,7 +16,7 @@ public class HelloServlet extends HttpServlet {
     {
         // Very simple - just return some plain text
         PrintWriter writer = response.getWriter();
-        writer.print("Hello World!!");
+        writer.print("Hello World from Srajan!!");
     }
 }
 
