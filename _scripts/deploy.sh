f545ed2162372ec61167e0e1b9137a4bc46c7d12
srajan96 <srajan1996@gmail.com>
2017-03-13 21:19:09 +0530

    changed path to ff

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -5,7 +5,7 @@ ssh -i travis deploy@139.59.61.26<<EOF
 cd feedfusion
 git pull
 mvn package
-cp target/hello.war /var/lib/tomcat8/webapps/
+cp target/hello.war /var/lib/tomcat8/webapps/feedfusion.war
 echo "deployed"
 EOF
 
