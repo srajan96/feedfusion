a44334c2d7a770384e511bc3cd7c624e02d5ea6e
srajan96 <srajan1996@gmail.com>
2017-03-13 12:33:50 +0530

    changed servlet contents

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -16,7 +16,7 @@ public class HelloServlet extends HttpServlet {
     {
         // Very simple - just return some plain text
         PrintWriter writer = response.getWriter();
-        writer.print("Hello Srajan 2!!");
+        writer.print("Hello Srajan,changed travis!!");
     }
 }
 
