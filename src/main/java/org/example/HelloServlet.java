6d082cf5be0ae54e405ad398fb3acff63eee1662
srajan96 <srajan1996@gmail.com>
2017-03-13 18:00:07 +0530

    ad

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Reflecting the directory structure where the file lives
 package org.example;
 
@@ -21,3 +22,28 @@ public class HelloServlet extends HttpServlet {
 }
 
 
+=======
+// Reflecting the directory structure where the file lives
+package org.example;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter; 
+
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException
+    {
+        // Very simple - just return some plain text
+        PrintWriter writer = response.getWriter();
+        writer.print("Srajan!Its finally done!!");
+    }
+}
+
+
+>>>>>>> parent of cfe43ec... TEST
