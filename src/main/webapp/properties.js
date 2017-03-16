1712622fc9bcb7d1e821857caace3e2661d8f1e5
Srajan Soni <sonisrajan96@gmail.com>
2017-03-16 10:21:16 +0530

    restored domain name as server url

--- src/main/webapp/properties.js
+++ src/main/webapp/properties.js
@@ -2,6 +2,6 @@
 //var sohagServerUrl = "http://localhost:8080/";
 
 //server url
-//var sohagServerUrl = "http://srajan96.ml:8080/";
-var sohagServerUrl = "http://139.59.61.26:8080/";
+var sohagServerUrl = "http://srajan96.ml:8080/feedfusion";
+//var sohagServerUrl = "http://139.59.61.26:8080/";
 
