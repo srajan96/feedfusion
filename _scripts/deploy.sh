4cbe65bf83a15d0ba569b2d4aab02d8ac83afe0f
Srajan Soni <sonisrajan96@gmail.com>
2017-03-16 18:09:39 +0530

    trying to resolve auto deploy error

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,7 +4,9 @@ chmod 600 deploy
 ssh -i deploy deploy@139.59.64.249<<EOF
 cd feedfusion
 git pull
+rm target/hello.war
 mvn package
+rm /var/lib/tomcat8/webapps/feedfusion.war
 cp target/hello.war /var/lib/tomcat8/webapps/feedfusion.war
 echo "deployed"
 EOF
