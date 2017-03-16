184cfb8918c8d9391215fdac047fa68242c9e084
Srajan Soni <sonisrajan96@gmail.com>
2017-03-16 10:23:30 +0530

    Updated root.js

--- src/main/webapp/script/services/root.js
+++ src/main/webapp/script/services/root.js
@@ -10,7 +10,7 @@ sohagApp.factory('SohagRootService', function ($http) {
 
             var req = {
                 method: 'POST',
-                url: sohagServerUrl + "getHomePageData",
+                url: sohagServerUrl + "/getHomePageData",
                 headers: {
                     'Content-Type': "text/html"
                 },
