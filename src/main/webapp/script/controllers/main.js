972530bf0ccd64d17f5ba80383b776e3af99e5aa
srajan96 <srajan1996@gmail.com>
2017-04-04 00:00:33 +0530

    deleted everything

--- src/main/webapp/script/controllers/main.js
+++
@@ -1,9 +0,0 @@
-var sohagApp = angular.module('SohagApp');
-
-sohagApp.controller("RootController",function($rootScope,$location, $scope) {
-  this.redirect = function(path){
-    console.log('In redirect '+ path);
-    $location.path(path);
-  }
-
-});
