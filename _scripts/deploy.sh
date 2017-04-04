a928579cdb392731d50469d04943fa44e78190f1
Travis Deploy <deploy@srajanubuntu.localdomain>
2017-04-04 11:03:33 +0000

    added permissions of foldeR?

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-rm deploy.enc
+rm  -r deploy.enc
 chmod 600 deploy
 ssh -i deploy deploy@139.59.64.249<<EOF
 cd feedfusion
 git pull
-rm target/hello.war
 mvn package
 rm /var/lib/tomcat8/webapps/feedfusion.war
-cp target/hello.war /var/lib/tomcat8/webapps/feedfusion.war
+cp target/feedfusiontest.war /var/lib/tomcat8/webapps/feedfusion.war
 echo "deployed"
 EOF
