9f068e7f30f60f6643bc6fb59325c1ed69cb7026
srajan96 <srajan1996@gmail.com>
2017-04-04 22:53:47 +0530

    changed deploy.sh to feedfusiontest

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -5,7 +5,7 @@ ssh -i deploy deploy@139.59.64.249<<EOF
 cd feedfusion
 git pull
 mvn package
-rm /var/lib/tomcat8/webapps/feedfusion.war
-cp target/feedfusiontest.war /var/lib/tomcat8/webapps/feedfusion.war
+rm /var/lib/tomcat8/webapps/feedfusiontest.war
+cp target/feedfusiontest.war /var/lib/tomcat8/webapps/feedfusiontest.war
 echo "deployed"
 EOF
